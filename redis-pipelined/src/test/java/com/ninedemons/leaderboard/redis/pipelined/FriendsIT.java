de2c2baebc6042845d96aa25dc6e7c2d232e39a4
Jon Barber <jon.barber@akqa.com>
2012-11-25 18:26:35 +0000

    Tests for multiple user id's

--- redis-pipelined/src/test/java/com/ninedemons/leaderboard/redis/pipelined/FriendsIT.java
+++ redis-pipelined/src/test/java/com/ninedemons/leaderboard/redis/pipelined/FriendsIT.java
@@ -3,12 +3,9 @@ package com.ninedemons.leaderboard.redis.pipelined;
 import com.ninedemons.leaderboard.api.Entry;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.*;
 
 /**
  * Created by Jon Barber
@@ -56,17 +53,49 @@ public class FriendsIT extends BaseRedisTest {
         assertNotNull("Should always return a List<Entry>", found);
         assertEquals("Should be one result", 1, found.size());
         assertEquals("Wrong user in result",HIGHEST_RANKED_USER,found.get(0).getUserId());
-        assertEquals("Wrong user in result",HIGHEST_RANKED_USER,found.get(0).getUserId());
         testEntriesAreCorrect(found, underTest.isUseZeroIndexForRank());
     }
 
     @Test
     public void testTwoUsers() {
-        List<Entry> found = underTest.friends(leaderboardName, Arrays.asList(LOWEST_RANKED_USER,HIGHEST_RANKED_USER));
+        Collection<String> friendsToFind = Arrays.asList(LOWEST_RANKED_USER,HIGHEST_RANKED_USER);
+        List<Entry> found = underTest.friends(leaderboardName, friendsToFind);
         assertNotNull("Should always return a List<Entry>", found);
         assertEquals("Should be two results", 2, found.size());
         testEntriesAreCorrect(found, underTest.isUseZeroIndexForRank());
         testEntriesAreOrdered(found);
+        testAllEntriesPresent(found,friendsToFind);
+    }
+
+    @Test
+    public void testThreeUsersOneDoesntExist() {
+        Collection<String> friendsToFind = Arrays.asList(LOWEST_RANKED_USER,NO_SUCH_USER,HIGHEST_RANKED_USER);
+        List<Entry> found = underTest.friends(leaderboardName, friendsToFind);
+        assertNotNull("Should always return a List<Entry>", found);
+        assertEquals("Should be two results", 2, found.size());
+        testEntriesAreCorrect(found, underTest.isUseZeroIndexForRank());
+        testEntriesAreOrdered(found);
+
+        friendsToFind.remove(NO_SUCH_USER);
+        testAllEntriesPresent(found,friendsToFind);
+    }
+
+    private void testAllEntriesPresent(List<Entry> foundEntries, Collection<String> idsToFind) {
+
+        Set<String> allFoundIds = new HashSet<String>(foundEntries.size());
+
+        for (Entry foundEntry: foundEntries) {
+            allFoundIds.add(foundEntry.getUserId());
+        }
+
+
+        for (String idToFind : idsToFind) {
+            assertTrue("Should have id " + idToFind + " in the returned list",allFoundIds.contains(idToFind));
+            allFoundIds.remove(idToFind);
+        }
+
+        assertTrue("ID(s) returned that wasn't in list to find " + allFoundIds,allFoundIds.isEmpty());
+
     }
 
 }
