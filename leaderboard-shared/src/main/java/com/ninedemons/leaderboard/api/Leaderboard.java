9f741fc65e8ff16ba91643904fae5be40c3086c6
Jon Barber <jon.barber@akqa.com>
2012-11-25 18:53:53 +0000

    Added some comments to the API

--- leaderboard-shared/src/main/java/com/ninedemons/leaderboard/api/Leaderboard.java
+++ leaderboard-shared/src/main/java/com/ninedemons/leaderboard/api/Leaderboard.java
@@ -6,10 +6,40 @@ import java.util.List;
 
 public interface Leaderboard {
 
+    /**
+     * For a given id return a list of entries centred around the id
+     *
+     * @param leaderboardName  The leaderboard to return results from
+     * @param userId  The id to centre the list around
+     * @return the list of entries surrounding the id
+     */
+
     List<Entry> aroundMe(String leaderboardName,String userId);
 
+    /**
+     * Returns a list of entries for the given list of ids. If an id supplied doesn't exist then no corresponding
+     * entry will be in the return value.
+     *
+     * @param leaderboardName The leaderboard to return results from
+     * @param userIds the list of ids to return results for
+     * @return a list of entries in descending order of rank - the first element will be the highest ranked.
+     */
     List<Entry> friends(String leaderboardName,Collection<String> userIds);
 
-    Page page(String leaderboardName,int page);
+    /**
+     * Returns a page of entries from the leaderboard.
+     *
+     * The lower the page number the higher the rank of the entries returned.
+     * If the page number is too low, e.g. -1, then the first lowest page will be returned - this will be the page of
+     * highest entries in the leader board.
+     *
+     * If the page is too high then the last page in the leader board will be returned - this will be the page of
+     * lowest entries in the leader board.
+     *
+     * @param leaderboardName the leader board to return results from
+     * @param pageNumber the page to return
+     * @return a page of data.
+     */
+    Page page(String leaderboardName,int pageNumber);
 
 }
